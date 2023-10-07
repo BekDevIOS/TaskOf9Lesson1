7c2244013a5e523ad82dd3fdb148cbb859b023c3
BekDevIOS <umekhmonboev@gmail.com>
2023-10-07 17:21:08 +0900

    Initial Commit

---
+++ TaskOf9Lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskOf9Lesson1
+//
+//  Created by Utkirbek Mekhmonboev on 10/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

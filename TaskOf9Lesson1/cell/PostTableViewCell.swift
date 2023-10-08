315c91c44b26e9e435296a212d32647d4f704bbd
BekDevIOS <umekhmonboev@gmail.com>
2023-10-08 20:32:00 +0900

    project updeted!

---
+++ TaskOf9Lesson1/cell/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  TaskOf9Lesson1
+//
+//  Created by Utkirbek Mekhmonboev on 10/8/23.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TaskOf9Lesson1/model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  TaskOf9Lesson1
+//
+//  Created by Utkirbek Mekhmonboev on 10/8/23.
+//
+
+import Foundation
